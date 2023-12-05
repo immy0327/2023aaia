6a02816003267b8bb10628e0bee120e22f85a184
immy0327 <12750776@me.mcu.edu.tw>
2023-12-05 10:27:57 +0800

    week13

---
+++ week13/week13-1b.py
@@ -0,0 +1,11 @@
+程式大會考
+分式化簡
+a, b  = list(map(int, input().split() ))
+
+def gcd(a, b):
+    if a==0: return b
+    if b==0: return a
+    return gcd(b, a%b)
+    
+ans = gcd(a,b)
+print( a//ans, b//ans )
